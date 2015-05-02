c2dc3cfc63eb4c81d8c3f3ab3205d987028648ed
Ruben Albor Cerda <trancemeister75@gmail.com>
2015-05-02 11:38:24 -0500

    U-113702052015
    - Se crea script de movimiento horizontal de plataformas así como
    también se afina el script de movimiento vertical.
    - Se termina prefab Simple Platform Level que sirve para dar estructura
    de escenario al Level 01.
    
    Se empieza Level 02

---
+++ RD001/Assets/Scripts/PlatformHController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformHController : MonoBehaviour {
+	
+	public GameObject platform;
+	public Vector2 direction;
+	public float x_position;
+	public float y_position;
+	public Vector2 pointA;
+	public Vector2 pointB;
+	public float offset;
+	public float platformSpeed;
+	public int delay;
+	// Use this for initialization
+	IEnumerator Start () {
+	 pointA = new Vector2(platform.transform.position.x + offset + offset,platform.transform.position.y);
+	 pointB = new Vector2(platform.transform.position.x - offset - offset,platform.transform.position.y);
+	 while(true)
+	 {
+		 yield return new WaitForSeconds(delay);
+		 yield return StartCoroutine(MovePlatform(platform.transform,pointA,pointB,platformSpeed));
+		 yield return StartCoroutine(MovePlatform(platform.transform,pointB,pointA,platformSpeed));
+	 }
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x_position = platform.transform.position.x;
+		y_position = platform.transform.position.y;
+	}
+	
+	IEnumerator MovePlatform(Transform thisTransform, Vector2 startPos, Vector2 endPos, float time)
+	{
+		var i=0.0f;
+		var rate=1.0f/time;
+		while(i<1.0f)
+		{
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector2.Lerp(endPos,startPos, i);
+			yield return null;
+		}
+	}
+}
+
